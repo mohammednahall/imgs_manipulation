693726f23533d0dfe4b8ab2d720f5875bd6b1ba2
mohammednahall <79056491+mohammednahall@users.noreply.github.com>
2021-12-22 17:48:01 +0200

    create src.java

--- src.java
+++ src.java
@@ -1,25 +1,35 @@
 package javaapplication1;
 
+// File class to get the image file from the user's pc
 import java.io.File;
 import java.io.IOException;
-import java.awt.image.BufferedImage;
+
+// ImageIO class to read and write the image data from and into the user's pc
 import javax.imageio.ImageIO;
+
+// BufferedImage class to store and manipulate the image data 
+import java.awt.image.BufferedImage;
 import java.util.Scanner;
 
 public class JavaApplication1 {
 
+    // create an object from BufferedImage class
     static BufferedImage img = null;
+    
+    // create an object from File class
     static File f = null;
+    
+    // initialize the width and height int variables
     static int width = 0, height = 0;
 
     public static void main(String[] args) {
 
-        //get the path from the user
+        // get the path from the user
         Scanner in = new Scanner(System.in);
         System.out.print("Enter the path of the image : ");
         String path = in.nextLine();
         
-        //read image
+        // read the image data
         try {
             f = new File(path);
             img = ImageIO.read(f);
@@ -30,14 +40,16 @@ public class JavaApplication1 {
             return;
         }
 
-        //get the name of the edited img that the user want to save
+        // get the name of the edited image that the user want to save
         System.out.print("Save the image as : ");
         String name = in.nextLine();
+        
+        // end the name with .jpg
         if (!name.endsWith(".jpg") && !name.endsWith(".png")) {
             name += ".jpg";
         }
 
-        //print all effects that user can choose from it
+        // print all effects that user can choose from it
         System.out.println("\nChoose one of the effects bellow :");
         System.out.println("1- contrast effect");
         System.out.println("2- brightness effect");
@@ -50,40 +62,78 @@ public class JavaApplication1 {
         System.out.print("Choose : ");
         
         int chsnEfct = in.nextInt();
+        
+        //-------------------------------------------------------------------------------------------------------------------------------------------------------------//
 
         if (chsnEfct == 1) {
-            System.out.print("\nEnter the effect's strong from 1 to 10 : ");
+            
+            // get the amount of the effect from the user
+            System.out.print("\nEnter the effect's strong from 1 to 100 : ");
             double amount = in.nextDouble()/100.0;
-            contrast(amount);            
+            
+            // apply the contrast effect 
+            contrast(amount);  
+            
         } else if (chsnEfct == 2) {
-            System.out.print("\nEnter the effect's strong from 1 to 10 : ");
-            double amount = in.nextDouble()/100.0;         
+            
+            // get the amount of the effect from the user
+            System.out.print("\nEnter the effect's strong from 1 to 100 : ");
+            double amount = in.nextDouble()/100.0;   
+            
+            // apply the brightness effect
             brightness(amount);
+            
         } else if (chsnEfct == 3) {
-            System.out.print("\nEnter the effect's strong from 1 to 10 : ");
-            double amount = in.nextDouble()/100.0;         
+            
+            // get the amount of the effect from the user
+            System.out.print("\nEnter the effect's strong from 1 to 100 : ");
+            double amount = in.nextDouble()/100.0;
+            
+            // apply the sunset effect
             sunsetEffect(amount);
+            
         } else if (chsnEfct == 4) {
-            System.out.print("\nEnter the effect's strong from 1 to 10 : ");
-            double amount = in.nextDouble()/100.0;         
+            
+            // get the amount of the effect from the user
+            System.out.print("\nEnter the effect's strong from 1 to 100 : ");
+            double amount = in.nextDouble()/100.0; 
+            
+            // apply the winter effect
             winterEffect(amount);
+            
         } else if (chsnEfct == 5) {
+            
+            // get the amount of the effect from the user
             System.out.print("\nEnter the percentage of radius to width : ");
             int r = (int)(in.nextDouble()/100.0*width);
             System.out.println("=> "+r+"px");
+            
+            // apply the tellscop effect
             tellscopEffect(r);
+            
         }else if (chsnEfct == 6){
+            
+            // apply the Black and White effect
             BWeffect();
+            
         }else if (chsnEfct == 7){
+            
+            // apply the negative effect
             negativeEffect();
+            
         }else if (chsnEfct == 8){
+            
+            // apply the rgb effect
             rgbEffect();
+            
         }
 
-        //save the img in the same pathof the original img
+        //-------------------------------------------------------------------------------------------------------------------------------------------------------------//        
+        
+        // save the image in the same path of the original img
         path = path.substring(0, path.lastIndexOf('\\') + 1) + name;
 
-        //write image
+        // create the file and write the image data into it
         try {
             f = new File(path);
             ImageIO.write(img, "jpg", f);
@@ -95,12 +145,19 @@ public class JavaApplication1 {
 
     }
     
-    //contrast effect
+    //-------------------------------------------------------------------------------------------------------------------------------------------------------------//
+    
+    // contrast effect
     private static void contrast(double amount){
+        
+        // nested for loop to loop in each pixel in the image
         for (int y = 0; y < height; y++) {
             for (int x = 0; x < width; x++) {
+                
+                // get the rgb hexadecimal value of the pixel in (x,y)
                 int p = img.getRGB(x, y);
 
+                // get the rgb decimal values
                 int r = (p >> 16) & 0xff;
                 int g = (p >> 8) & 0xff;
                 int b = p & 0xff;
@@ -155,22 +212,28 @@ public class JavaApplication1 {
                     if(b > 255)b = 255;
                 }
 
+                // set the rgb values of the pixel in hexadecimal system
                 img.setRGB(x, y, (255 << 24) | (r << 16) | (g << 8) | b);
             }
         }
     }
     
-    //brightness effect
+    // brightness effect
     private static void brightness(double amount){
+        
+        // nested for loop to loop in each pixel in the image
         for (int y = 0; y < height; y++) {
             for (int x = 0; x < width; x++) {
+                
+                // get the rgb hexadecimal value of the pixel in (x,y)
                 int p = img.getRGB(x, y);
 
+                // get the rgb decimal values
                 int r = (p >> 16) & 0xff;
                 int g = (p >> 8) & 0xff;
                 int b = p & 0xff;
 
-                //increase the rgb value of each pixel
+                // increase the rgb value of each pixel
                 r += amount*r;
                 if(r>255)r = 255;
                 g += amount*g;
@@ -178,69 +241,92 @@ public class JavaApplication1 {
                 b += amount*b;
                 if(b>255)b = 255;
 
+                // set the rgb values of the pixel in hexadecimal system
                 img.setRGB(x, y, (255 << 24) | (r << 16) | (g << 8) | b);
             }
         }
     }
 
-    //sunse effect
+    // sunset effect
     private static void sunsetEffect(double amount) {
+        
+        // nested for loop to loop in each pixel in the image
         for (int y = 0; y < height; y++) {
             for (int x = 0; x < width; x++) {
+                
+                // get the rgb hexadecimal value of the pixel in (x,y)
                 int p = img.getRGB(x, y);
 
+                // get the rgb decimal values
                 int r = (p >> 16) & 0xff;
                 int g = (p >> 8) & 0xff;
                 int b = p & 0xff;
                 
-                //increase the red value of each pixel
+                // increase the red value of each pixel
                 if (r + amount * r < 255) {
                     r += amount * r;
                 } else {
                     r = 255;
                 }
 
+                // set the rgb values of the pixel in hexadecimal system
                 img.setRGB(x, y, (255 << 24) | (r << 16) | (g << 8) | b);
             }
         }
     }
 
-    //winter effect
+    // winter effect
     private static void winterEffect(double amount) {
+        
+        // nested for loop to loop in each pixel in the image
         for (int y = 0; y < height; y++) {
             for (int x = 0; x < width; x++) {
+                
+                // get the rgb hexadecimal value of the pixel in (x,y)
                 int p = img.getRGB(x, y);
 
+                // get the rgb decimal values
                 int r = (p >> 16) & 0xff;
                 int g = (p >> 8) & 0xff;
                 int b = p & 0xff;
 
-                //increase the blue value of each pixel
+                // increase the blue value of each pixel
                 if (b + amount * b < 255) {
                     b += amount * b;
                 } else {
                     b = 255;
                 }
 
+                // set the rgb values of the pixel in hexadecimal system
                 img.setRGB(x, y, (255 << 24) | (r << 16) | (g << 8) | b);
             }
         }
     }
 
-    //tellscop effect
+    // tellscop effect
     private static void tellscopEffect(int r) {
+        
+        // nested for loop to loop in each pixel in the image
         for (int y = 0; y < height; y++) {
+            
             int Y = (height - y) - height / 2;
             if (Y < -r || Y > r) {
+                
                 for (int x = 0; x < width; x++) {
+                    
+                    // set the rgb values of the pixel in hexadecimal system
                     img.setRGB(x, y, (255 << 24) | (0 << 16) | (0 << 8) | 0);
                 }
             }
             else {
+                
                 double a = Math.asin((double)Y/r);
                 for (int x = 0; x < width; x++) {
+                    
                     int X = x - width / 2;
                     if (X < -Math.cos(a)*r|| X > Math.cos(a)*r) {
+                        
+                        // set the rgb values of the pixel in hexadecimal system
                         img.setRGB(x, y, (255 << 24) | (0 << 16) | (0 << 8) | 0);
                     }
                 }
@@ -248,44 +334,61 @@ public class JavaApplication1 {
         }
     }
     
-    //black and white effect
+    // Black and White effect
     private static void BWeffect() {
+        
+        // nested for loop to loop in each pixel in the image
         for (int y = 0; y < height; y++) {
             for (int x = 0; x < width; x++) {
+                
+                // get the rgb hexadecimal value of the pixel in (x,y)
                 int p = img.getRGB(x, y);
 
+                // get the rgb decimal values
                 int r = (p >> 16) & 0xff;
                 int g = (p >> 8) & 0xff;
                 int b = p & 0xff;
 
                 int bw = Math.max(r, Math.max(g, b));
 
+                // set the rgb values of the pixel in hexadecimal system
                 img.setRGB(x, y, (255 << 24) | (bw << 16) | (bw << 8) | bw);
             }
         }
     }
 
-    //negative effect
+    // negative effect
     private static void negativeEffect() {
+        
+        // nested for loop to loop in each pixel in the image
         for (int y = 0; y < height; y++) {
             for (int x = 0; x < width; x++) {
+                
+                // get the rgb hexadecimal value of the pixel in (x,y)
                 int p = img.getRGB(x, y);
 
+                // get the rgb decimal values
                 int r = (p >> 16) & 0xff;
                 int g = (p >> 8) & 0xff;
                 int b = p & 0xff;
 
+                // set the rgb values of the pixel in hexadecimal system
                 img.setRGB(x, y, (255 << 24) | (255 - r << 16) | (255 - g << 8) | 255 - b);
             }
         }
     }
     
-    //rgb effect
+    // rgb effect
     private static void rgbEffect(){
+        
+        // nested for loop to loop in each pixel in the image
         for (int y = 0; y < height; y++) {
             for (int x = 0; x < width; x++) {
+                
+                // get the rgb hexadecimal value of the pixel in (x,y)
                 int p = img.getRGB(x, y);
 
+                // get the rgb decimal values
                 int r = (p >> 16) & 0xff;
                 int g = (p >> 8) & 0xff;
                 int b = p & 0xff;
@@ -301,6 +404,7 @@ public class JavaApplication1 {
                     r = 0; g = 0;
                 }
 
+                // set the rgb values of the pixel in hexadecimal system
                 img.setRGB(x, y, (255 << 24) | (r << 16) | (g << 8) | b);
             }
         }
